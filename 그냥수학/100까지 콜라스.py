581e354a04abfb34c7a7182314b0f306dbdf51d6
BearMeal <uk2465@naver.com>
2022-11-03 10:06:00 +0900

    wers

--- 그냥수학/100까지 콜라스.py
+++ 그냥수학/100까지 콜라스.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 
-t=[i for i in range(1,101)]
+print("안녕하세요.콜라스의 추측게산기 최대값 그래프입니다. 원하는 n~k 범위의 값을 넣으면 최대 높이들을 볼 수 있습니다.")
+print('원하는 범위 : '); n,k=map(int,input().split())
+
+t=[i for i in range(n,k+1)]
 
 a=[]
-a.append(x)
+a.append(x) 
 for x in t:
     while True:
         if x%2 == 1:
@@ -25,6 +28,7 @@ for x in t:
             if x==1:
                 print('콜라츠의 추측이 맞습니다.')
                 break
+
             
 plt.plot(a,'-')
 plt.xlabel('account')
