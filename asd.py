0e4bf44ac4081441cf25dc1870c5858e0e55c3df
BearMeal <uk2465@naver.com>
2022-11-29 10:45:18 +0900

    save

--- asd.py
+++
@@ -1,9 +0,0 @@
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.get('http://www.naver.com')
-
